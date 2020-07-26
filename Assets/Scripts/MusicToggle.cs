1c356cc2f74a27917225b07bd6664fc9a3192458
Fedron <40535546+Fedron@users.noreply.github.com>
2020-07-26 19:42:52 +0100

    Sound and Music Toggles

---
+++ Assets/Scripts/MusicToggle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicToggle : MonoBehaviour {
+    [SerializeField] Toggle toggle;
+
+    private void Start() {
+        if (PlayerPrefs.GetFloat("music vol") == 0f) {
+            toggle.isOn = true;
+        } else {
+            toggle.isOn = false;
+        }
+    }
+
+    public void ToggleMusic() {
+        if (toggle.isOn) {
+            AudioManager.Instance.SetVolume(0f, AudioManager.AudioChannel.Music);
+        } else {
+            AudioManager.Instance.SetVolume(1f, AudioManager.AudioChannel.Music);
+        }
+    }
+}
---
+++ Assets/Scripts/SoundToggle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundToggle : MonoBehaviour {
+    [SerializeField] Toggle toggle;
+
+    private void Start() {
+        if (PlayerPrefs.GetFloat("sfx vol") == 0f) {
+            toggle.isOn = false;
+        } else {
+            toggle.isOn = true;
+        }
+    }
+
+    public void ToggleSound() {
+        if (toggle.isOn) {
+            AudioManager.Instance.SetVolume(1f, AudioManager.AudioChannel.Sfx);
+        } else {
+            AudioManager.Instance.SetVolume(0f, AudioManager.AudioChannel.Sfx);
+        }
+    }
+}
